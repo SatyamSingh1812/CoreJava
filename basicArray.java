657d31eb2665a178e0f2d0524cd24793f63da622
Satyam Singh <Satyamsingh1812@gmail.com>
2024-06-12 00:15:39 +0530

    this is just a array implementation code

---
+++ basicArray.java
@@ -0,0 +1,8 @@
+public class basicArray {
+    public static void main(String[] args) {
+        int num[]={4,6,7,8,2,3,};
+        for (int j : num) {
+            System.out.print("[" + ++j + "]");
+        }
+    }
+}
