4cd8a189ae08f51c5047554f5d5b1da501215485
Satyam Singh <Satyamsingh1812@gmail.com>
2024-06-13 10:05:51 +0530

    This is the basic code for multidimensional array

--- Array/MultiDim.java
+++ Array/MultiDim.java
@@ -5,12 +5,23 @@ public class MultiDim {
 
 
     int multi[][]=new int [3][4];
-    for (int i =0 ;i<3 ; i++){
-        for (int j = 0; j < 4; j++) {
-            System.out.print(multi[i][j]+ " ");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                //assigning random value to multi[][]
+                multi[i][j]= (int)(Math.random()* 10);
+                System.out.print(multi[i][j]+" ");
+
+            }
+            System.out.println();
+
         }
-        System.out.println();
-    }
+//    for (int i =0 ;i<3 ; i++){
+//        for (int j = 0; j < 4; j++) {
+//            System.out.print(multi[i][j]+ " ");
+//        }
+//        System.out.println();
+//    }
     }
 
 }
